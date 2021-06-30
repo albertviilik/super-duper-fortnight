d9b0b51f8a078ef5b19075cc22e466136aea977c
Albert Viilik <42554671+albertviilik@users.noreply.github.com>
2021-06-30 20:23:44 +0100

    Third Commit

--- service2/src/App.tsx
+++ service2/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1>First Column</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer arcu purus, fermentum quis venenatis id, ultricies sed nisl. Vestibulum a nisi faucibus, vulputate tellus vulputate, placerat tortor. Maecenas urna ante, pretium vel ex at, faucibus posuere ipsum. Fusce porttitor neque vel risus suscipit, sed mollis quam dapibus. Suspendisse hendrerit tortor a lectus aliquam, eget mattis est bibendum. Integer in turpis semper nunc iaculis interdum. Sed viverra, lacus in lacinia efficitur, arcu justo mollis elit, posuere aliquam magna nisl vitae felis. Duis nisl nisi, luctus id erat sed, consequat luctus nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus ipsum massa, varius eget tincidunt vestibulum, malesuada nec mauris. Etiam laoreet imperdiet felis, sed maximus est semper et. Fusce nec dapibus augue. Ut lacus augue, consectetur vel magna vitae, mattis pretium justo. Sed sapien risus, ornare quis felis non, posuere bibendum neque.</p>
+          </div>
+          <div className="col-md-6">
+            <h1>Second Column</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer arcu purus, fermentum quis venenatis id, ultricies sed nisl. Vestibulum a nisi faucibus, vulputate tellus vulputate, placerat tortor. Maecenas urna ante, pretium vel ex at, faucibus posuere ipsum. Fusce porttitor neque vel risus suscipit, sed mollis quam dapibus. Suspendisse hendrerit tortor a lectus aliquam, eget mattis est bibendum. Integer in turpis semper nunc iaculis interdum. Sed viverra, lacus in lacinia efficitur, arcu justo mollis elit, posuere aliquam magna nisl vitae felis. Duis nisl nisi, luctus id erat sed, consequat luctus nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus ipsum massa, varius eget tincidunt vestibulum, malesuada nec mauris. Etiam laoreet imperdiet felis, sed maximus est semper et. Fusce nec dapibus augue. Ut lacus augue, consectetur vel magna vitae, mattis pretium justo. Sed sapien risus, ornare quis felis non, posuere bibendum neque.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
